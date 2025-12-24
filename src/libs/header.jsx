a2fc1541fee498758e419b604b8f9b48c78687b1
hxscy <hxscyprodz@gamil.com>
2025-12-24 18:56:18 +0200

    added responsiveness

--- src/libs/header.jsx
+++ src/libs/header.jsx
@@ -1,11 +1,12 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Link as ScrollLink } from "react-scroll"
+import { Menu } from 'lucide-react'
 
 export default function Header() {
   return (
     <header
       className="flex items-center justify-between fixed top-0 bg-black 
-        left-0 right-0 h-20 px-20 z-50"
+        left-0 right-0 h-20 px-5 z-50"
     >
       <div className="flex flex-col items-center justify-center">
         <img src="/blackciti.svg" alt="" />
@@ -13,6 +14,9 @@ export default function Header() {
         <p className="text-gray-200 italic text-[10px]">
           Best investment solutions
         </p>
+      </div>
+      <div className='lg:hidden'>
+        <Menu color='white' />
       </div>
       <nav className="hidden sm:hidden lg:flex gap-7 text-gray-200 ">
         <ScrollLink
